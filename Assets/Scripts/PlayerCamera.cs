1392f27659ae5a10b4682ae60b8bc1e87607dbc4
NwitherellYYE <103435089+NwitherellYYE@users.noreply.github.com>
2022-07-07 16:28:01 -0400

    some changes

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,18 +1,35 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
+
+    public GameObject target;
+
+    
+
+
+
+    private float offset = 50f;
+
+    public float SmoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = target.transform.position - target.transform.forward * offset;
+        transform.LookAt(target.transform.position);
+        transform.position = new Vector3(transform.position.x, transform.position.y + 15, transform.position.z );
     }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
+    [SerializeField]
+    private int topSpeed;
+
+    Rigidbody rb;
+    Vector3 movement;
+    private int currentSpeed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     //  movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
     }
+
+    void FixedUpdate()
+    {
+      //  Driving(movement);
+    }
+
+    void Driving(Vector3 direction)
+    {
+       // direction = rb.rotation * direction;
+        while (Input.GetKeyDown(KeyCode.W)){
+            
+            
+            if (currentSpeed < topSpeed)
+            {
+
+
+              // rb.MovePosition(rb.position+direction*currentSpeed*Time.fixedDeltaTime);
+                currentSpeed++;
+                Debug.Log(getSpeed());
+            }   
+        }
+
+
+    }
+
+
+   public int getSpeed()
+    {
+        return currentSpeed;
+    }
+
 }
